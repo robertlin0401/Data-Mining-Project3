73ca76f96e3fcb3fb086cab15e606c61cae75d98
robertlin0401 <robertlin0401@gmail.com>
2022-01-09 17:47:42 +0800

    Others: revise some comments

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-# read graph data at {path} and return an adjacent matrix of that graph
+# Read graph data at {path} and return an adjacent matrix of that graph.
 def readGraph(path):
     lines = []
     max_node_id = 0
 
-    # read lines and find the max node id
+    # Read lines and find the max node id.
     with open(path, 'r') as fd:
         for line in fd.read().splitlines():
             lines.append(line)
@@ -13,7 +13,7 @@ def readGraph(path):
                 max_node_id = max(int(line.split(',')[0]), int(line.split(',')[1]))
         fd.close()
     
-    # build the adjacent matrix
+    # Build the adjacent matrix.
     adj_matrix = np.zeros((max_node_id, max_node_id))
     for line in lines:
         adj_matrix[int(line.split(',')[0]) - 1][int(line.split(',')[1]) - 1] = 1
