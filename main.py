c390db5d224826ab2f34f000cf1db43d99c2432e
robertlin0401 <robertlin0401@gmail.com>
2022-01-09 19:22:29 +0800

    Revise: save the results of HITS

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import numpy as np
 from src import dataloader, HITS
 
 # constant
-path = "./data/"
+path = {
+    "input": "./data/",
+    "output": "./output/"
+}
 graphs = [
     "graph_1.txt",
     "graph_2.txt",
@@ -15,8 +19,8 @@ ibm = "ibm-5000.txt"
 # Read data.
 matrices = []
 for graph in graphs:
-    matrices.append(dataloader.readGraph(path + graph))
-matrices.append(dataloader.readIBM(path + ibm))
+    matrices.append(dataloader.readGraph(path["input"] + graph))
+matrices.append(dataloader.readIBM(path["input"] + ibm))
 
 # HITS
 a = []
@@ -25,3 +29,11 @@ for matrix in matrices:
     result = HITS.run(matrix)
     a.append(result[0])
     h.append(result[1])
+
+for i in range(7):
+    if i != 6:
+        np.savetxt(path["output"] + graphs[i].split('.')[0] + "_HITS_authority.txt", a[i], fmt="%.8f", newline=" ")
+        np.savetxt(path["output"] + graphs[i].split('.')[0] + "_HITS_hub.txt", h[i], fmt="%.8f", newline=" ")
+    else:
+        np.savetxt(path["output"] + ibm.split('.')[0] + "_HITS_authority.txt", a[i], fmt="%.8f", newline=" ")
+        np.savetxt(path["output"] + ibm.split('.')[0] + "_HITS_hub.txt", h[i], fmt="%.8f", newline=" ")
